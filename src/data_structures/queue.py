d52a9d7969baee374b3778c64d83c3783719dc4b
EddieEngineers <eddieschaumanhaigh@gmail.com>
2023-05-26 13:16:36 -0700

    Add first/last functions

--- src/data_structures/queue.py
+++ src/data_structures/queue.py
@@ -1,11 +1,17 @@
 class Queue:
     _queue = []
 
-    def enqueue(self, value) -> None:
-        self._queue.append(value)
-    
     def dequeue(self) -> any:
         return self._queue.pop(0)
+
+    def enqueue(self, value) -> None:
+        self._queue.append(value)
+        
+    def first(self) -> any:
+        return self._queue[0]
+    
+    def last(self) -> any:
+        return self._queue[-1]
 
     def __len__(self):
         return len(self._queue)
